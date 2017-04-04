2c9746f4bf6abeccd4305191f1d3f1fc88eb6844
Chris Flowers <clflowers5@gmail.com>
2017-04-03 18:56:52 -0600

    block it spec and package.json test

--- test/blockItSpec.js
+++ test/blockItSpec.js
@@ -8,20 +8,20 @@ const paceIt = require('../blockIt').paceIt;
 const testArr = [...Array(500).keys()];
 
 function arraysAreEqual(arr1, arr2) {
-  let equalLength = arr1.length == arr2.length;
+  let equalLength = arr1.length === arr2.length;
   let equalContents = arr1.every((val, idx) => val === arr2[idx]);
   return equalLength && equalContents;
 }
 
-test('blockIt performs synchronously', function () {
-  const testFunc = function(index) {
+test('blockIt performs synchronously', function (next) {
+  const testFunc = function (index) {
     return new Promise(function (resolve) {
       resolve(index);
     });
   };
 
   blockIt(testFunc, testArr)
-    .then(function(result) {
+    .then(function (result) {
       ok(arraysAreEqual(result, testArr), 'Tasks are performed in order and block until resolved.');
       next();
     });
